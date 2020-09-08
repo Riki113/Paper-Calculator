6f626124bad5b058956bb29d7373b76d1046a19c
Riki113 <shinobi.1341@gmail.com>
2020-09-08 14:52:13 +0900

    fix 登録と削除ボタンをより丸く fix キャッシュバージョン更新

--- docs/service_worker.js
+++ docs/service_worker.js
@@ -1,5 +1,5 @@
 // キャッシュファイルの指定
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
 const urlsToCache = [
     '.',
@@ -9,10 +9,10 @@ const urlsToCache = [
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
+        .open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
@@ -20,9 +20,9 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
+        .match(event.request)
+        .then(function(response) {
+            return response ? response : fetch(event.request);
+        })
     );
 });
